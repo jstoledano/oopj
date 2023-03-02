6e651694724df422c7cb48940c75b31bd5dd96bf
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2023-03-01 20:35:44 -0600

    Part 4, ready

--- src/org/toledano/duke/Main.java
+++ src/org/toledano/duke/Main.java
@@ -1,13 +1,17 @@
 package org.toledano.duke;
 
 import org.toledano.duke.week2.Part3;
+import org.toledano.duke.week2.Part4;
 
 public class Main {
     public static void main(String[] args) {
 //        Part2 gn = new Part2("ACG", "CTA");
 //        gn.testSimpleGene();
 
-        Part3 p3 = new Part3();
-        p3.testing();
+//        Part3 p3 = new Part3();
+//        p3.testing();
+
+        Part4 p4 = new Part4();
+        p4.findLink("youtube");
     }
 }
--- src/org/toledano/duke/week2/Part4.java
+++ src/org/toledano/duke/week2/Part4.java
@@ -1,7 +1,20 @@
 package org.toledano.duke.week2;
 
-
+import edu.duke.URLResource;
 
 public class Part4 {
+    private static String url = "https://www.dukelearntoprogram.com//course2/data/manylinks.html";
 
+    public void findLink(String link) {
+        URLResource res = new URLResource(url);
+
+        for (String word : res.words()) {
+            int startIndex = word.indexOf("://");
+            int endIndex = word.indexOf("\">");
+            boolean isYoutube = word.contains("youtube.com");
+            if (isYoutube) {
+                System.out.println(word.substring(startIndex+3, endIndex));
+            }
+        }
+    }
 }
